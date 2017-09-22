9f2277b549df864bea2b55771976e5682c67ddbe
zhouhaiming <joamingzhm@163.com>
2017-09-22 14:20:22 +0800

    title

--- app/src/main/java/com/example/joaming/firstweather/mainActivity.java
+++ app/src/main/java/com/example/joaming/firstweather/mainActivity.java
@@ -1,17 +1,24 @@
 package com.example.joaming.firstweather;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by joaming on 2017/9/20.
  */
 
-public class mainActivity extends Activity{
+public class mainActivity extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
     }
+
 }
