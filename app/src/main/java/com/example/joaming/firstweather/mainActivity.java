9f26889040771f009812499df096a9d1d31c2bbb
zhouhaiming <joamingzhm@163.com>
2017-10-18 16:21:16 +0800

    20171018

--- app/src/main/java/com/example/joaming/firstweather/mainActivity.java
+++ app/src/main/java/com/example/joaming/firstweather/mainActivity.java
@@ -2,6 +2,7 @@ package com.example.joaming.firstweather;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
@@ -36,6 +37,8 @@ public class mainActivity extends Activity implements View.OnClickListener {
     private static final int UPDATE_TODAY_WEATHER =1;
 
     private ImageView mUpdateBtn;
+    private ImageView mCitySelect;
+
     private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv,
             temperatureTv, climateTv, windTv, city_name_Tv,fengxiangTv;
     private ImageView weatherImg, pmImg;
@@ -60,6 +63,9 @@ public class mainActivity extends Activity implements View.OnClickListener {
         mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
         mUpdateBtn.setOnClickListener(this);
 
+        mCitySelect = (ImageView)findViewById(R.id.title_city_manager);
+        mCitySelect.setOnClickListener(this);
+
         if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
             Log.d("myWeather", "网络OK");
             Toast.makeText(mainActivity.this,"网络OK！", Toast.LENGTH_LONG).show();
@@ -73,24 +79,41 @@ public class mainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-
-        if (view.getId() == R.id.title_update_btn){
+        if (view.getId() == R.id.title_city_manager) {
+            Intent i = new Intent(this, SelectCity.class);
+            //startActivity(i);
+            startActivityForResult(i,1);
+        }
+        if (view.getId() == R.id.title_update_btn) {
 
             SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
-            String cityCode = sharedPreferences.getString("main_city_code","101010100");
-            Log.d("myWeather",cityCode);
+            String cityCode = sharedPreferences.getString("main_city_code", "101010100");
+            Log.d("myWeather", cityCode);
 
 
             if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
                 Log.d("myWeather", "网络OK");
                 queryWeatherCode(cityCode);
-            }else
-            {
+            } else {
                 Log.d("myWeather", "网络挂了");
-                Toast.makeText(mainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
+                Toast.makeText(mainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
             }
         }
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String newCityCode= data.getStringExtra("cityCode");
+            Log.d("myWeather", "选择的城市代码为"+newCityCode);
+            if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+                Log.d("myWeather", "网络OK");
+                queryWeatherCode(newCityCode);
+            } else {
+                Log.d("myWeather", "网络挂了");
+                Toast.makeText(mainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 
     /**
      *
@@ -357,15 +380,5 @@ public class mainActivity extends Activity implements View.OnClickListener {
         fengxiangTv.setText(todayWeather.getFengxiang());
         Toast.makeText(mainActivity.this,"更新成功！",Toast.LENGTH_SHORT).show();
     }
-
-
-
-
-
-
-
-
-
-
 
 }
